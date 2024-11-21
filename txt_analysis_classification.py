9fb808ea853c40affca1b416c24b276fef1181b7
BALASURYA <kuttypulisurya22@gmail.com>
2024-11-21 15:01:19 +0530

    added to detect fraud's

--- txt_analysis_classification.py
+++ txt_analysis_classification.py
@@ -27,7 +27,7 @@ for char in compliant:
         temp_compliant += char
     else:
         temp_compliant += ' '
-        
+
 compliant = temp_compliant
 
 # converting the compliant into words
@@ -36,23 +36,26 @@ splitted_compliant = list(compliant.split())
 # removing tenses form
 temp_splitted_compliant = []
 for index, word in enumerate(splitted_compliant):
-    
+
     if word[-3:] == 'ing':
         temp_splitted_compliant.insert(index, word[:-3])
-    
+
     elif word[-2:] == 'ed' or word[-2:] == 'es':
         temp_splitted_compliant.insert(index, word[:-2])
-    
+
     elif word[-1:] == 's' and not word[-2:] == 'ss':
         temp_splitted_compliant.insert(index, word[:-1])
-    
+
     else:
         temp_splitted_compliant.insert(index, word)
 
 splitted_compliant = temp_splitted_compliant
 
 # creating an data set for joining words
-join_words = ['he', 'same', 'as', 'on', 'o', 'off', 'is', 'during', 'was', 'out', 'such', 'into', 'll', 'doing', 'where', 'shouldn', 'after', 'wouldn', 'the', 'couldn', 'because', 'hers', 've', "should've", 'him', 'who', 'my', "it's", 'herself', 'mightn', 'there', "you'd", 'most', 're', 'am', 'before', "shouldn't", 'through', 'not', "didn't", 'below', 'about', 'in', 'over', 'can', 'our', "weren't", 'has', 'once', 'by', 'at', 'here', 'just', 'why', "hasn't", 'too', "isn't", "won't", 'don', 'are', 'mustn', 'any', 'have', 'down', 'her', 'again', 'should', 'being', 'each', 'will', "you'll", 'while', "wouldn't", 'an', 'doesn', 'were', "couldn't", 'now', 'ma', 'myself', 'won', 'you', 'them', 'yourselves', "you've", 'she', 'd', "shan't", 'when', 'few', 'his', "haven't", 'or', 'be', 'other', 'yourself', 'yours', 'didn', 'for', 'its', 'your', 'm', 'some', 'but', 'than', 'ours', "aren't", 'all', 'itself', "she's", 'if', 't', "hadn't", "mightn't", 'weren', 'very', 'then', 'theirs', 'up', 'to', 'own', 'more', "needn't", 'had', 'i', 'themselves', 'did', 'these', 'their', 's', 'ourselves', 'under', 'me', "you're", 'those', 'wasn', "don't", 'of', 'isn', 'against', 'and', 'from', 'himself', 'hadn', "doesn't", 'what', 'this', 'hasn', 'no', 'with', 'both', 'y', 'between', 'whom', 'until', "that'll", 'how', 'only', 'further', 'a', 'that', 'do', 'they', 'needn', 'above', "mustn't", 'which', 'aren', 'it', 'so', 'nor', 'shan', 'ain', 'haven', 'does', "wasn't", 'we', 'having', 'been']
+join_words = ['he', 'same', 'as', 'on', 'o', 'off', 'is', 'during', 'was', 'out', 'such', 'into', 'll', 'doing', 'where', 'shouldn', 'after', 'wouldn', 'the', 'couldn', 'because', 'hers', 've', "should've", 'him', 'who', 'my', "it's", 'herself', 'mightn', 'there', "you'd", 'most', 're', 'am', 'before', "shouldn't", 'through', 'not', "didn't", 'below', 'about', 'in', 'over', 'can', 'our', "weren't", 'has', 'once', 'by', 'at', 'here', 'just', 'why', "hasn't", 'too', "isn't", "won't", 'don', 'are', 'mustn', 'any', 'have', 'down', 'her', 'again', 'should', 'being', 'each', 'will', "you'll", 'while', "wouldn't", 'an', 'doesn', 'were', "couldn't", 'now', 'ma', 'myself', 'won', 'you', 'them', 'yourselves', "you've", 'she', 'd', "shan't", 'when', 'few', 'his', "haven't", 'or', 'be', 'other', 'yourself', 'yours', 'didn', 'for', 'its', 'your', 'm', 'some', 'but', 'than', 'ours', "aren't", 'all', 'itself', "she's", 'if', 't', "hadn't", "mightn't", 'weren', 'very', 'then', 'theirs', 'up', 'to', 'own', 'more', "needn't", 'had', 'i', 'themselves', 'did', 'these', 'their', 's', 'ourselves', 'under', 'me', "you're", 'those', 'wasn', "don't", 'of', 'isn', 'against', 'and', 'from', 'himself', 'hadn', "doesn't", 'what', 'this', 'hasn', 'no', 'with', 'both', 'y', 'between', 'whom', 'until', "that'll", 'how', 'only', 'further', 'a', 'that', 'do', 'they', 'needn', 'above', "mustn't", 'which', 'aren', 'it', 'so', 'nor', 'shan', 'ain', 'haven', 'does', "wasn't", 'we', 'having', 'been'
+
+
+]
 
 # removing joining words from the splitted_compliant variable
 for join_word in join_words:
@@ -62,32 +65,35 @@ for join_word in join_words:
 # Identify the type of cyber crime 
 detect_fraud = []
 for word in splitted_compliant:
+    
     for crime,keywords in fraud_dataset.items():
-        count = 0
+        count =0
         for key in keywords:
             if key == word:
                 count += 1
         if count > 0:
             detect_fraud.append([crime,count])
 
+print(detect_fraud)
 # detect the fraud
-_fraud=[]
+_fraud=""
 _fcount=0
 for crime,count in detect_fraud:
+    if count>0:  
+        print(crime,count)
     if _fcount < count:
-        _fraud.insert(0,crime)
-        del _fraud[1]
+        _fraud = crime
         _fcount = count
     elif _fcount == count:
-        _fraud.inset(1,crime)
+        _fraud += " or " + crime
         _fcount = count
     else:
         continue
 
-    
+
 
 
 
 # displaying the output
-print(f"detected cyber crime fraud : {_fraud}")
+print(f"detected cyber crime fraud \n -> {_fraud}")
 #print(f"detected victim : {detected_victim}")
