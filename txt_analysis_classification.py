5d796548633e28fbb492da078c47c0fee24b7601
ALONE-VIBER <kuttypulisurya22@gmail.com>
2024-11-21 05:51:08 +0530

    added code to remove the tenses

--- txt_analysis_classification.py
+++ txt_analysis_classification.py
@@ -20,9 +20,26 @@ for char in compliant:
 compliant = temp_compliant
 
 # converting the compliant into words
-splitted_compliant = compliant.split()
+splitted_compliant = list(compliant.split())
 
-for word in splitted_compliant
+# removing tenses form
+temp_splitted_compliant = []
+for index, word in enumerate(splitted_compliant):
+    
+    if word[-3:] == 'ing':
+        temp_splitted_compliant.insert(index, word[:-3])
+    
+    elif word[-2:] == 'ed' or word[-2:] == 'es':
+        temp_splitted_compliant.insert(index, word[:-2])
+    
+    elif word[-1:] == 's' and not word[-2:] == 'ss':
+        temp_splitted_compliant.insert(index, word[:-1])
+    
+    else:
+        temp_splitted_compliant.insert(index, word)
+
+splitted_compliant = temp_splitted_compliant
+
 # creating an data set for joining words
 join_words = ['he', 'same', 'as', 'on', 'o', 'off', 'is', 'during', 'was', 'out', 'such', 'into', 'll', 'doing', 'where', 'shouldn', 'after', 'wouldn', 'the', 'couldn', 'because', 'hers', 've', "should've", 'him', 'who', 'my', "it's", 'herself', 'mightn', 'there', "you'd", 'most', 're', 'am', 'before', "shouldn't", 'through', 'not', "didn't", 'below', 'about', 'in', 'over', 'can', 'our', "weren't", 'has', 'once', 'by', 'at', 'here', 'just', 'why', "hasn't", 'too', "isn't", "won't", 'don', 'are', 'mustn', 'any', 'have', 'down', 'her', 'again', 'should', 'being', 'each', 'will', "you'll", 'while', "wouldn't", 'an', 'doesn', 'were', "couldn't", 'now', 'ma', 'myself', 'won', 'you', 'them', 'yourselves', "you've", 'she', 'd', "shan't", 'when', 'few', 'his', "haven't", 'or', 'be', 'other', 'yourself', 'yours', 'didn', 'for', 'its', 'your', 'm', 'some', 'but', 'than', 'ours', "aren't", 'all', 'itself', "she's", 'if', 't', "hadn't", "mightn't", 'weren', 'very', 'then', 'theirs', 'up', 'to', 'own', 'more', "needn't", 'had', 'i', 'themselves', 'did', 'these', 'their', 's', 'ourselves', 'under', 'me', "you're", 'those', 'wasn', "don't", 'of', 'isn', 'against', 'and', 'from', 'himself', 'hadn', "doesn't", 'what', 'this', 'hasn', 'no', 'with', 'both', 'y', 'between', 'whom', 'until', "that'll", 'how', 'only', 'further', 'a', 'that', 'do', 'they', 'needn', 'above', "mustn't", 'which', 'aren', 'it', 'so', 'nor', 'shan', 'ain', 'haven', 'does', "wasn't", 'we', 'having', 'been']
 
